aa6b653246a8d84be464e02ef3e0612cff995d52
automate2020 <74991209+automate2020@users.noreply.github.com>
2021-04-03 07:45:38 +0000

    a

---
+++ ahref.js
@@ -0,0 +1,12 @@
+{
+@ahref.js {
+  <a href="./home/kali/Desktop/app-store-connect-openapi-spec.json"></a>
+  <a href="./unpkg.com/systemjs@0.19.47/dist/system.js"></a>
+  <a href='https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js'></a>
+  <a href="./text/javascript/js/controller.js"</a>
+  <a href="./assets/js/jquery.min.js"></a>
+  <a href="./assets/js/bootstrap.min.js"></a>
+  <a href="./assets/js/angular.min.js"></a>
+  <a href="./sytlesheet.css"</a>
+  }
+}
